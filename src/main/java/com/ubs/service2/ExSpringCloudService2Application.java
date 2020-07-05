094ff718ecdd1c59993cd73f4b88143572dd3deb
sumitkch <sumitcs0041@gmail.com>
2020-07-05 20:17:46 +0530

    first commit

---
+++ src/main/java/com/ubs/service2/ExSpringCloudService2Application.java
@@ -0,0 +1,13 @@
+package com.ubs.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringCloudService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringCloudService2Application.class, args);
+	}
+
+}
